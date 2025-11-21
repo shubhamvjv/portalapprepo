99cbf4ebd32d7d012e4a202facc74cd2109f5d05
shubhamvjv <shubham.vijayvargiya@celebaltech.com>
2025-11-21 16:28:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,10 +39,5 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
 
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
+
