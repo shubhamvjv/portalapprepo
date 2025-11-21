ce59a4bfd0d6aac4b253dffa8f3d0532c01f0972
Backstage <backstage@example.com>
2025-11-21 14:50:21 +0530

    Initial commit from Backstage

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.65"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+# -------------------------------------------------------
+# RESOURCE GROUP (Terraform will manage it)
+# -------------------------------------------------------
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# -------------------------------------------------------
+# STORAGE ACCOUNT FOR FUTURE USE (Optional Infra)
+# -------------------------------------------------------
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "${var.app_service_name}sa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
